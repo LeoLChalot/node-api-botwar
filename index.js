ba9644417829b0b0dc8e836b732503fb5124c8ff
LeoLChalot <l.larouchalot@gmail.com>
2025-07-30 10:30:48 +0200

    Création de la route /action

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors())
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');
 });
 
+
+/*  
+*   # Reçoit l'état de la partie
+*
+*   @param {object} state - L'état de la partie
+*   @return {JSON} action - L'action à effectuer
+*/
+app.post('/action', (req, res) => {
+    const state = req.body.state;
+    console.log('/action')
+    console.log(state ? state : 'aucun état reçu');
+})
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 });
