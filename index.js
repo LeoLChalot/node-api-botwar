7410da232813fbdb455bdd8a5bfcbed0ad9b87a7
LeoLChalot <l.larouchalot@gmail.com>
2025-07-30 12:39:21 +0200

    Mise à jour de key/values dans vercel.json

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const app = express();
 
 const port = 3000;
 
-app.use(cors({
-  origin: 'https://bot.gogokodo.com'
-}));
 
-// app.get('/', (req, res, next) => {
-//     res.send('Hello World!');
-// });
 
+const whitelist = ['https://bot.gogokodo.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.json({message: "Welcome bot trainer"}).status(200);
+})
 
 /*  
 *   # Reçoit l'état de la partie
@@ -19,7 +29,7 @@ app.use(cors({
 *   @param {object} state - L'état de la partie
 *   @return {JSON} action - L'action à effectuer
 */
-app.get('/', (req, res) => {
+app.get('/action', (req, res) => {
     console.log('/action')
     const state = req.headers['X-Game-State'];
     console.log(state ? state : 'aucun état reçu');
